d0fc2a497e58614f14e20277cdc720643071ce5c
Valerko <Valerko@192.168.1.7>
2023-07-02 10:47:24 +0300

    Upload Lesson_01

---
+++ Lesson_01/task_2/task_02.js
@@ -0,0 +1,13 @@
+let admin;
+let password;
+
+admin = "Admin";
+password = "TheMaster";
+
+if (admin == "Admin") {
+	if (password == "TheMaster") { console.log("Welcome!"); }
+	else if (password == undefined) { console.log("Canceled"); }
+	else console.log("Wrong passwod");
+}
+else if (admin == undefined) console.log("Canceled");
+else console.log("I don't know you");
